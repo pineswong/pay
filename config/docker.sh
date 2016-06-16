3d9eb213c03988a95dd4e7d8b3066c5759cbd802
PineWong <PineWong@163.com>
2016-06-16 14:02:33 +0800

    init

--- config/docker.sh
+++ config/docker.sh
@@ -12,11 +12,11 @@
 
 # RUN gem update --system
 echo '/*** Gem Source:';
-gem sources -r https://rubygems.org/ -a https://gems.ruby-china.org/;
+# gem sources -r https://rubygems.org/ -a https://gems.ruby-china.org/;
 gem source;
 
 echo '/*** Bundle Install:';
-bundle config mirror.https://rubygems.org https://gems.ruby-china.org;
+# bundle config mirror.https://rubygems.org https://gems.ruby-china.org;
 bundle install;
 
 
@@ -29,10 +29,15 @@ rake db:create;
 rake db:migrate;
 rake db:seed;
 
-# docker rm -f webxx; docker run --name webxx --link postgres -v /home/apps/webxx:/home/apps/web -p 80:80 -d pinewong/passenger-ruby22;
+# docker rm -f postgres; docker run --name postgres -p 5432:5432 -d postgres
 
-# docker rm -f webxx; docker run --name webxx --link postgres -v /Users/mac/apps/web/webxx:/home/apps/web -p 80:80 -d pinewong/passenger-ruby22;
+# docker rm -f web; docker run --name web --link postgres -v /home/apps/pay:/home/apps/web -p 80:80 -d pinewong/passenger-ruby22;
+
+# docker rm -f web; docker run --name webxx --link postgres -v /Users/mac/apps/web/webxx:/home/apps/web -p 80:80 -d pinewong/passenger-ruby22;
+
+
 
 # docker rmi -f pinewong/passenger-ruby22; docker build -t pinewong/passenger-ruby22 .;
 
+# docker logs -f web;
 
